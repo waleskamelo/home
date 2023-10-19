591ea02f4df10ced0c2a6cd7f6feeaa77dd85338
Waleska Melo <smelowaleska@gmail.com>
2023-10-19 16:20:21 +0200

    main carousel activating arrows

--- js/script.js
+++ js/script.js
@@ -12,7 +12,8 @@
         lazyLoad: true,
         loop:true,
         margin:10,
-        nav: true
+        nav: true,
+        navText: ['<i class="fa-solid fa-chevron-left"></i>','<i class="fa-solid fa-chevron-right"></i>']
     });
 
  });
