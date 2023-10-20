aab29b27c1834f7134165c5909245167848e25ce
Waleska Melo <smelowaleska@gmail.com>
2023-10-20 11:12:12 +0200

    main carousel speed

--- js/script.js
+++ js/script.js
@@ -7,14 +7,19 @@
     }); 
 
     //CAROUSEL INDEX
-    $('.owl-carousel').owlCarousel({
+    $('#carousel_main').owlCarousel({
         items:1,
         lazyLoad: true,
         loop:true,
         margin:10,
         nav: true,
+        navSpeed: 1500,
         navText: ['<i class="fa-solid fa-chevron-left"></i>','<i class="fa-solid fa-chevron-right"></i>'],
-        dots: true
+        dots: true,
+        dotsSpeed:1500,
+        autoplay: false,
+        autoplaySpeed: 1500,
+        responsiveRefreshRate: 10
     });
 
  });
