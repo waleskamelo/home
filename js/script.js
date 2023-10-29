be892fe692cb850b4ebe09cd92feb6cc35ea593c
Waleska Melo <smelowaleska@gmail.com>
2023-10-29 20:38:32 +0100

    removed carousel

--- js/script.js
+++ js/script.js
@@ -6,20 +6,20 @@
         $('.header-collapse').toggleClass('active');
     }); 
 
-    //CAROUSEL INDEX
-    $('#carousel_main').owlCarousel({
-        items:1,
-        lazyLoad: true,
-        loop:true,
-        margin:10,
-        nav: true,
-        navSpeed: 1500,
-        navText: ['<i class="fa-solid fa-chevron-left"></i>','<i class="fa-solid fa-chevron-right"></i>'],
-        dots: true,
-        dotsSpeed:1500,
-        autoplay: false,
-        autoplaySpeed: 1500,
-        responsiveRefreshRate: 10
-    });
+    // //CAROUSEL INDEX
+    // $('#carousel_main').owlCarousel({
+    //     items:1,
+    //     lazyLoad: true,
+    //     loop:true,
+    //     margin:10,
+    //     nav: true,
+    //     navSpeed: 1500,
+    //     navText: ['<i class="fa-solid fa-chevron-left"></i>','<i class="fa-solid fa-chevron-right"></i>'],
+    //     dots: true,
+    //     dotsSpeed:1500,
+    //     autoplay: false,
+    //     autoplaySpeed: 1500,
+    //     responsiveRefreshRate: 10
+    // });
 
  });
