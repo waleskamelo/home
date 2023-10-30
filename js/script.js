876f08f14a2f84ad7a6296a8efab44da78495ef1
Waleska Melo <smelowaleska@gmail.com>
2023-10-30 11:22:18 +0100

    code cleaning

--- js/script.js
+++ js/script.js
@@ -1,25 +1,9 @@
  $(document).ready(function(){
-    //MENU
+    /*----- MENU -----*/
     $('#nav-toggle').on('click', function(e) {
         e.preventDefault();
         $(this).toggleClass('active');
         $('.header-collapse').toggleClass('active');
     }); 
-
-    // //CAROUSEL INDEX
-    // $('#carousel_main').owlCarousel({
-    //     items:1,
-    //     lazyLoad: true,
-    //     loop:true,
-    //     margin:10,
-    //     nav: true,
-    //     navSpeed: 1500,
-    //     navText: ['<i class="fa-solid fa-chevron-left"></i>','<i class="fa-solid fa-chevron-right"></i>'],
-    //     dots: true,
-    //     dotsSpeed:1500,
-    //     autoplay: false,
-    //     autoplaySpeed: 1500,
-    //     responsiveRefreshRate: 10
-    // });
 
  });
